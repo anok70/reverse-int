0ace58652b70031f52937555d9eaf429a30975d6
anok70 <anok70@mail.ru>
2022-08-06 17:08:50 +0300

    completed task for reverse-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+    let str;
+    let n1;
+    str = n.toString();
+
+    if (n <0) {
+        strRevers = str.slice(1).split('').reverse().join('');
+        n1 = Number(strRevers);
+    }
+    else {
+        strRevers = str.split('').reverse().join('');
+        n1 = Number(strRevers);
+    }
+
+    return n1;
 }
